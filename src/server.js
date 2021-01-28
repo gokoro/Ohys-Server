a5843b4f7117d5d6cee89b05b421578f376fb5ba
Gokoro <lincroe@gmail.com>
2021-01-29 00:18:41 +0900

    Add database connection into the server execution

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const { port } = require('./config')
+const db = require('./loaders/mongoose')
 const app = require('./app')
+
+db.connect()
 
 app.listen(port, () => {
   console.log(`The server is running on port ${port}!`)
