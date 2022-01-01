c486227fd1f83693c7ba182eb19d84cf0371346f
Gokoro <lincroe@gmail.com>
2022-01-01 18:52:46 +0900

    Change the bundler from Webpack to esbuild

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const slsw = require('serverless-webpack')
-const nodeExternals = require('webpack-node-externals')
-
-module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  externals: [nodeExternals()],
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-}
